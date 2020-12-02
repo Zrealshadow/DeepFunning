5c44210a6b30ea57a0be5f930da4ada540e7e3d0
Zreal <zenglz_pro@163.com>
2020-12-02 10:43:20 +0800

    update baseWrapper

--- deepfunning/base/basewrapper.py
+++ deepfunning/base/basewrapper.py
@@ -16,64 +16,64 @@ from deepfunning.base.basedataloader import BaseDataLoader
 class BaseWrapper(object):
     """Base class for Wrapper
     """
-    def self(self,model:nn.Module, config:Dict):
-        super(self,BaseWrapper).__self__()
-
+    def __init__(self,model:nn.Module, config:Dict):
+        super(BaseWrapper,self).__init__()
         self.required_mode = [
             'loss_fn', 'checkpoint_epoch', 'print_step', 'batch_size', 'lr', 'checkpoint_dir', 'cuda',
-            'optim'
+            'optim', 'epoches'
             ]
-        F = lambda key:self.getconfigattr(key,config)
+    
+        F = lambda key: self.getconfigattr(key,config)
+        
         self.model = model
         self.device = torch.device(config['cuda']) if torch.cuda.is_available() else torch.device('cpu')
         
-        self.loss_fn = self.__getconfigattr('loss_fn')
+        self.loss_fn = F('loss_fn')
         self.start_epoch = 0
-        self.checkpoint_epoch = self.getconnfigattr('checkpoinnt_epoch')
-        self.print_step = self.getconnfigattr('print_step')
-        self.init_lr = self.getconnfigattr('lr')
+        self.epoches = F('epoches')
+        self.checkpoint_epoch = F('checkpoint_epoch')
+        self.print_step = F('print_step')
+        self.init_lr = F('lr')
         self.best_model = self.model
-        self.batch_size = self.getconnfigattr('batch_size')
-        self.checkpoint_dir = self.getconnfigattr('checkpoint_dir')
-        
+        self.batch_size = F('batch_size')
+        self.checkpoint_dir = F('checkpoint_dir')
+
         #data_loader
-        self.train_dataloader = self.getconfigattr('train_dataloader')
-        self.dev_dataloader = self.getconfigattr('dev_dataloader')
-        self.test_dataloader = self.getconfigattr('test_dataloader')
+        self.train_dataloader = F('train_dataloader')
+        self.dev_dataloader = F('dev_dataloader')
+        self.test_dataloader = F('test_dataloader')
 
         #subclass have to overwrite
-        self.optimizer = self.getconfigattr('optim')
+        self.optimizer = F('optim')
         self.best_score = 0.0
-
+        
     def getconfigattr(self,key:str,config:Dict)->Any:
         if key not in config:
             if key in self.required_mode:
                 raise ValueError('Attribute {} is required in this model wrapper'.format(key))
             else:
-                warnings.warn('Attribute {} is not in config'.format(key))
-        return config.get(key, default=None)
-            
+                desc = 'Attribute {} is not in config'.format(key)
+                warnings.warn(desc)
+                return None
+        return config[key]
 
-    def train(self):
+
+    def train(self,train_dataloader:BaseDataLoader, dev_dataloader:Optional[BaseDataLoader])->None:
+        raise NotImplementedError()       
+
+    def train_(self):
         raise NotImplementedError()
     
-    @__t.overload
-    def train(self,train_dataloader:BaseDataLoader, dev_dataloader:Optional[BaseDataLoader]):
-        raise NotImplementedError()
-    
+
     def validation(self):
         raise NotImplementedError()
-    
-    @__t.overload
-    def validation(self,dev_dataloader:BaseDataLoader):
+
+    def test_performance(self,test_dataloader:BaseDataLoader)->None:
         raise NotImplementedError()
 
-    def test_performance(self):
+    def test_performance_(self):
         raise NotImplementedError()
     
-    @__t.overload
-    def test_performance(self):
-        raise NotImplementedError()
 
     def save_check_point(self, filename:str = None, epoch:int = None) -> None:
         """ save the key attribute in this wrapper
@@ -90,12 +90,12 @@ class BaseWrapper(object):
         now = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
         if filename is None:
             if epoch is None:
-                filename = self.model.name+'_'+self.model.version+'_'+now+'_checkpoint.pth.tar'
+                filename = self.model.name+'_'+self.model.version+'_'+'best_model'+'_checkpoint.pth.tar'
             filename = self.model.name+'_'+self.model.version+'_'+'epoch:'+str(epoch)+'_'+now+'_checkpoint.pth.tar'
         
         filepath = Path.joinpath(dir_path, filename)
         d = {
-            'epoch':self.start_epoches,
+            'epoch':self.start_epoch,
             'state_dict' : self.model.state_dict(),
             'optimizer' : self.optimizer.state_dict(),
             'best_model' : self.best_model.state_dict(),
